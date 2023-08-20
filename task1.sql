fec49a924f94006bf350858069502941438164dc
vbutkov <bytkov@mail.ru>
2023-08-20 11:41:59 +0300

    Delete task1.sql

--- task1.sql
+++
@@ -1,9 +0,0 @@
--- Вывести имя владельца аккаунта, которому чаще всего переводили деньги
-
-select u.name, tr.to_account as account_id, count(*) as transfer_count 
-from transfer tr
-left join account acc on acc.account_id = tr.to_account
-left join users u on u.user_id = acc.user_id
-group by tr.to_account, u.name
-order by transfer_count desc 
-limit 1;
